bdc84ae9e487304eb563a51ddae070c18eaaf1c3
Marcos Marinho <marcosmarinho@santos.sp.gov.br>
2026-06-18 13:07:56 -0300

    att repositório e crud completo

---
+++ src/main/java/com/example/trabalhoP1/TrabalhoP1Application.java
@@ -0,0 +1,13 @@
+package com.example.trabalhoP1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabalhoP1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabalhoP1Application.class, args);
+	}
+
+}
